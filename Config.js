ddbbd52a608e9f527715fcbfe2f85d08c03821a0
MohdNasser-Center <mohddnasser@gmail.com>
2025-04-16 17:03:36 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,3 +1,3 @@
-const TOKEN_PART1 = "ghp_21Ejborpxj7NkR5Ndam";
-const TOKEN_PART2 = "6Fg2zHE5FWb2M7fn7";
+const TOKEN_PART1 = "ghp_p978SjipcrsACpRwZm";
+const TOKEN_PART2 = "81e1r4Zkm7pv3vEa2e";
 const GITHUB_TOKEN = TOKEN_PART1 + TOKEN_PART2;
